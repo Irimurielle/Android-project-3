84a8e5d90146c20dcf0402d049dec6d776db9e28
Irimurielle <irimurielle@gmail.com>
2021-01-31 19:15:39 +0200

    added login activity

---
+++ app/src/main/java/com/irimurielle/foodrecipes2/ui/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.irimurielle.foodrecipes2.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
