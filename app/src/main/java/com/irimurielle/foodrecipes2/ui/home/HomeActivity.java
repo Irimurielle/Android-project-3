8fd4f6191d957a366f4fb3fd95ce980730bfe7d4
Irimurielle <irimurielle@gmail.com>
2021-02-03 13:23:11 +0200

    modified home activity

--- app/src/main/java/com/irimurielle/foodrecipes2/ui/home/HomeActivity.java
+++ app/src/main/java/com/irimurielle/foodrecipes2/ui/home/HomeActivity.java
@@ -2,6 +2,9 @@ package com.irimurielle.foodrecipes2.ui.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,10 +13,13 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.irimurielle.foodrecipes2.R;
 import com.irimurielle.foodrecipes2.Utils;
 import com.irimurielle.foodrecipes2.adapters.*;
 import com.irimurielle.foodrecipes2.models.*;
+import com.irimurielle.foodrecipes2.ui.LoginActivity;
 import com.irimurielle.foodrecipes2.ui.category.CategoryActivity;
 import com.irimurielle.foodrecipes2.ui.detail.DetailActivity;
 
@@ -28,12 +34,12 @@ public class HomeActivity extends AppCompatActivity implements HomeView {
     public static final String EXTRA_CATEGORY = "category";
     public static final String EXTRA_POSITION = "position";
     public static final String EXTRA_DETAIL = "detail";
+    private FirebaseAuth mAuth;
 
     @BindView(R.id.viewPagerHeader)
     ViewPager viewPagerMeal;
     @BindView(R.id.recyclerCategory)
     RecyclerView recyclerViewCategory;
-
     HomePresenter presenter;
 
     @Override
@@ -41,10 +47,37 @@ public class HomeActivity extends AppCompatActivity implements HomeView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
 
         presenter = new HomePresenter(this);
         presenter.getMeals();
         presenter.getCategories();
+        FirebaseUser user = mAuth.getCurrentUser();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_detail, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
     @Override
